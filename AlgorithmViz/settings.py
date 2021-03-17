39bf81d3594b520062a1ee4bd1187938f7c3ce44
Nishant <nishantnks@hotmail.com>
2021-03-17 12:13:12 +0530

    Hide-Configs-Secret-Key: Fixed Open/Visible password.

--- AlgorithmViz/settings.py
+++ AlgorithmViz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .secret_settings import SecretSetting
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gyx13g=#9p4-df#ales%@0upjvys7l^2($y+m!0%dla5@#jcjw'
+secret_setting= SecretSetting()
+SECRET_KEY = SecretSetting().get_secret_setting()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
