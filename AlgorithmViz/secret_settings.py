024d352ad244e32029b60c687062f5216c9f60bb
Nishant <nishantnks@hotmail.com>
2021-03-17 12:26:11 +0530

    Hide-Configs-Secret-Key: Fixed Open/Visible password.

---
+++ AlgorithmViz/secret_settings.py
@@ -0,0 +1,6 @@
+class SecretSetting:
+    def __init__(self):
+        self.__secret_pass = 'gyx13g=#9p4-df#ales%@0upjvys7l^2($y+m!0%dla5@#jcjw'
+
+    def get_secret_setting(self):
+        return self.__secret_pass
